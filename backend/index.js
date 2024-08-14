c8d45027d884366ec1583b3fd703d6e14113ca40
ccristianss <cristian.silva9402@gmail.com>
2024-08-13 22:12:31 -0500

    Identificacion con cuenta de Gmail

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,66 @@
 const express = require("express");
+const crypto = require('crypto');
+const session = require('express-session');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 
 const PORT = 3000;
+const state = crypto.randomBytes(32).toString('hex');
+
+app.use(
+  session({
+    secret: state,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new GoogleStrategy(
+    {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      return done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    console.log(req.user.id);
+    res.redirect("/");
+  }
+);
+
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);

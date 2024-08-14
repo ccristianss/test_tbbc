2222d5d3320109b47d576d8ec24610d36d58292e
ccristianss <cristian.silva9402@gmail.com>
2024-08-13 23:40:09 -0500

    detalles del clima con ventana emergente

--- Frontend/client/src/components/WeatherList.js
+++ Frontend/client/src/components/WeatherList.js
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { useState }  from "react";
+import WeatherDetailsModal from "./WeatherDetailsModal";
 
 function WeatherList({ cities, removeCity }) {
-    return (
-        <div>
-            {cities.map((city, index) => (
-                <div key={index} className="city-card">
-                    <h3>{city.name}</h3>
-                    <p>Temperatura: {city.main.temp}°C</p>
-                    <p>Clima: {city.weather[0].description}</p>
-                    <button onClick={() => removeCity(index)}>Eliminar</button>
-                </div>
-            ))}
+
+  const [selectedCity, setSelectedCity] = useState(null);
+
+  const handleCityClick = (city) => {
+    setSelectedCity(city);
+  };
+
+  const closeModal = () => {
+    setSelectedCity(null);
+  };
+
+  return (
+    <div>
+      {cities.map((city, index) => (
+        <div
+          key={index}
+          className="city-card"
+          onClick={() => handleCityClick(city)}
+        >
+          <h3>{city.name}</h3>
+          <p>Temperatura: {city.main.temp}°C</p>
+          <p>Clima: {city.weather[0].description}</p>
+          <button
+            onClick={(e) => {
+              e.stopPropagation();
+              removeCity(index);
+            }}
+          >
+            Eliminar
+          </button>
         </div>
-    );
+      ))}
+      <WeatherDetailsModal
+        isOpen={!!selectedCity}
+        onRequestClose={closeModal}
+        city={selectedCity}
+      />
+    </div>
+  );
 }
 
 export default WeatherList;
